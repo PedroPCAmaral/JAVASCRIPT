adb74f498c30354ca17264a1daeab43974d90208
Pedro Amaral <pedropaulocostadoamaral@gmail.com>
2025-09-04 16:40:03 -0300

    Semáforo

---
+++ semaforo.js
@@ -0,0 +1,18 @@
+
+let corSemaforo = "verde"; 
+
+
+if (corSemaforo === "verde") {
+    console.log("Siga em frente!");
+
+} else if (corSemaforo === "amarelo") {
+
+    console.log("Atenção! Prepare-se para parar.");
+
+} else if (corSemaforo === "vermelho") {
+    console.log("Pare!");
+
+} else {
+    console.log("Cor inválida.");
+
+}
