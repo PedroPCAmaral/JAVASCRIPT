cd29b59aeb14ba8aa1f293688bb03fc5a8db824d
Pedro Amaral <pedropaulocostadoamaral@gmail.com>
2025-09-04 17:41:57 -0300

    Calculadora

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+let num1 = 10;
+let num2 = 0;
+let operacao = "/";
+
+if (operacao === "+") {
+console.log("Resultado: ", (num1 + num2));
+
+} else if (operacao === "-")  {
+    console.log("Resultado: " + (num1 - num2));
+
+}else if (operacao === "/") {
+    console.log("Resultado: " + (num1 / num2));
+
+} else if (operacao === "*") {
+console.log("Resultado: " + (num1 * num2));
+
+} if (num2 !== 0) {
+console.log("Resultado: " + (num1 / num2));
+
+} else {
+    console.log("Erro")
+}
